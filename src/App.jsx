b2ddbd3da3839adda81ec062173ec386288f7463
jlordbucayu <105701528+jlordbucayu@users.noreply.github.com>
2024-02-13 22:44:17 +0800

    update

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import imageSrc from './assets/flower-gentleman.gif';
+import kissSrc from './assets/kiss.gif';
+import kapSrc from './assets/kap.png';
+import './App.css';
+
+function App() {
+  const [yesSize, setYesSize] = useState(16);
+  const [showMessage, setShowMessage] = useState(false);
+  const [showQuestion, setShowQuestion] = useState(true);
+  const [noButtonText, setNoButtonText] = useState("No");
+  const [noButtonColor, setNoButtonColor] = useState("red");
+  const noButtonOptions = [
+    "No",
+    "Please",
+    "Joke mo lang diba?",
+    "Wrong click?",
+    "sige naa. sige na plss",
+    "plsssssssss",
+    "Click mo yung YES, Sabi yung Yes, yung Color greeen"
+  ];
+  const [noButtonIndex, setNoButtonIndex] = useState(0);
+
+  const handleNoClick = () => {
+    setYesSize(yesSize + 10);
+    setShowMessage(false);
+
+    let newIndex = noButtonIndex + 1;
+    if (newIndex >= noButtonOptions.length) {
+      newIndex = 0; // Reset to the beginning if end of options reached
+    }
+
+    setNoButtonIndex(newIndex);
+    setNoButtonText(noButtonOptions[newIndex]);
+
+    if (newIndex === 7) {
+      setNoButtonColor("green"); // Change color only when reaching the end of options
+    } else {
+      setNoButtonColor("red");
+    }
+  };
+
+  const handleYesClick = () => {
+    setYesSize(100);
+    setShowMessage(true);
+    setShowQuestion(false);
+  };
+
+  return (
+    <>
+      <div>
+        {showQuestion && (
+          <div style={{ marginBottom: '20px' }}>
+            <img src={imageSrc} alt="Your Image" style={{ width: '200px' }} />
+            <h2>Will you be my Valentine?</h2>
+            <div>
+              <button onClick={handleYesClick} style={{ fontSize: yesSize, backgroundColor: 'green', color: 'white', marginRight: '10px' }}>
+                Yes
+              </button>
+              <button onClick={handleNoClick} style={{ backgroundColor: noButtonColor, color: 'white' }}>{noButtonText}</button>
+            </div>
+          </div>
+        )}
+        {showMessage && (
+          <div style={{ marginTop: '20px' }}>
+            <img src={kissSrc} alt="Your Image" style={{ width: '200px' }} />
+            <h2>Yay, I love you! Muah!</h2>
+            {/* <img src={kapSrc} alt="Your Image" style={{ width: '200px' }} />
+            <h2>Ulul pakyu!</h2> */}
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
